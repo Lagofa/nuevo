d5df3ccb361d9fcc1c447a01db2f93c0453eb187
Fabian <falago@gmail.com>
2017-11-07 18:14:57 -0300

    h7

--- src/main/webapp/scripts/app/offers/offers.controller.js
+++ src/main/webapp/scripts/app/offers/offers.controller.js
@@ -3,10 +3,18 @@
 angular.module('jobvacancyApp')
     .controller('OffersController', function ($scope, Offer, ParseLinks) {
         $scope.jobOffers = [];
+        $scope.word = " ";
+        $scope.focus = false;
+        $scope.regex = /\w+(\s\w+)*/g;
         $scope.page = 0;
+        $scope.searching = false;
+        $scope.myFunct = function(keyEvent) {
+  			if (keyEvent.which === 13)
+    			$scope.search();
+		};
         $scope.loadAll = function() {
-            Offer.query({page: $scope.page, size: 20}, function(result, headers) {
-                $scope.links = ParseLinks.parse(headers('link'));
+            Offer.query({page:$scope.page, size:20, word:$scope.word, isSearch:$scope.searching}, function(result, headers) {
+            	$scope.links = ParseLinks.parse(headers('link'));
                 $scope.jobOffers = result;
             });
         };
@@ -14,6 +22,12 @@ angular.module('jobvacancyApp')
             $scope.page = page;
             $scope.loadAll();
         };
+        $scope.search = function(page) {
+        	$scope.searching = true;
+            $scope.page = page;
+            $scope.loadAll();
+        };
+        
         $scope.loadAll();
 
     });
